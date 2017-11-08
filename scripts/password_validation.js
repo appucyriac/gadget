704bd8ec06b61a41af20f6033163bd2a7cc55bbb
appucyriac <appu@qburst.com>
2017-11-08 18:13:47 +0530

    validation added gh pages

---
+++ scripts/password_validation.js
@@ -0,0 +1,32 @@
+var password = document.getElementById("password")
+var confirm_password = document.getElementById("confirm_password");
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+       confirm_password.setCustomValidity("Passwords Don't Match");
+  } 
+  else{ 
+
+  	 if(password.value =="" || confirm_password.value ==""){
+       confirm_password.setCustomValidity("Please fill in all fields");
+  	  } 
+  	  else{
+            confirm_password.setCustomValidity("Passwords match");
+  	  	    modal.style.display = "block";
+            span.onclick = function() {
+            modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+             if (event.target == modal) {
+              modal.style.display = "none";
+  	          }
+
+            }
+        }
+    }
+}
+
