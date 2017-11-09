a14c26e18183011698d67e6fb600d066a63938ca
appucyriac <appu@qburst.com>
2017-11-09 11:19:20 +0530

    form validation

--- scripts/password_validation.js
+++ scripts/password_validation.js
@@ -4,7 +4,7 @@ var email = document.getElementById("email")
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function validatePassword()
 {  if(password.value != confirm_password.value) {
@@ -13,7 +13,7 @@ function validatePassword()
   else{ 
     if (reg.test(email) == false) 
             {
-                confirm_password.setCustomValidity("Invalid email");
+                email.setCustomValidity("Invalid email");
                 return (false);
             }
     else{
