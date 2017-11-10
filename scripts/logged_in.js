62d493629902bd3dd747b7d12e2e84a19f672bce
appucyriac <appu@qburst.com>
2017-11-10 12:45:42 +0530

    sign in aded only for user id and pass combo

--- scripts/logged_in.js
+++ scripts/logged_in.js
@@ -4,25 +4,24 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
-function logged(){
+function logged() {
 
-        if(password.value=="" || email.value=="")
-        {
-        	password.setCustomValidity("Fill in all fields");
-   
-        }
-        else{
-            password.setCustomValidity("Password Correct");
-            modal.style.display = "block";
-            span.onclick = function() {
+    if (password.value == "123" && email.value == "appu@qburst.com") {
+        password.setCustomValidity("Password Correct");
+        modal.style.display = "block";
+        span.onclick = function() {
             modal.style.display = "none";
-            }
+        }
 
-            window.onclick = function(event) {
-             if (event.target == modal) {
-              modal.style.display = "none";
-  	          }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+
+    } else {
+
+        password.setCustomValidity("Invalid User ID/Password");
     }
-   
- }
 }
