629eb76bcb9f697cc79e78aa7d14426631a7badf
appucyriac <appu@qburst.com>
2017-11-10 15:21:06 +0530

    email validation and page redirection added

--- scripts/logged_in.js
+++ scripts/logged_in.js
@@ -4,25 +4,29 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
-function logged(){
+function logged() {
 
-        if(password.value=="" || email.value=="")
-        {
-        	password.setCustomValidity("Fill in all fields");
-   
-        }
-        else{
-            password.setCustomValidity("Password Correct");
-            modal.style.display = "block";
-            span.onclick = function() {
+    if (password.value == "123" && email.value == "appu@qburst.com") {
+        password.setCustomValidity("You will be redirected shortly");
+        modal.style.display = "block";
+        span.onclick = function() {
             modal.style.display = "none";
-            }
+        }
 
-            window.onclick = function(event) {
-             if (event.target == modal) {
-              modal.style.display = "none";
-  	          }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        setTimeout(
+            function() {
+                window.location = 'https://appucyriac.github.io/gadget/index.html'
+            }, 2500);
+
+
+
+    } else {
+
+        password.setCustomValidity("Invalid User ID/Password");
     }
-   
- }
 }
--- scripts/password_validation.js
+++ scripts/password_validation.js
@@ -1,32 +1,46 @@
 var password = document.getElementById("password")
 var confirm_password = document.getElementById("confirm_password");
+var email = document.getElementById("email");
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+var check = /(.+)@(.+){2,}\.(.+){2,}/;
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-       confirm_password.setCustomValidity("Passwords Don't Match");
-  } 
-  else{ 
+function validatePassword() {
 
-  	 if(password.value =="" || confirm_password.value ==""){
-       confirm_password.setCustomValidity("Please fill in all fields");
-  	  } 
-  	  else{
-            confirm_password.setCustomValidity("Passwords match");
-  	  	    modal.style.display = "block";
-            span.onclick = function() {
-            modal.style.display = "none";
-            }
+  var em =document.getElementById("email").value;
 
-            window.onclick = function(event) {
-             if (event.target == modal) {
-              modal.style.display = "none";
-  	          }
+  if (check.test(em) == false) {
+    email.setCustomValidity("Invalid email");
+    return (false);
+  } else {
 
-            }
+    if (password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+
+
+      if (password.value == "" || confirm_password.value == "" || email.value == "") {
+        confirm_password.setCustomValidity("Please fill in all fields");
+      } else {
+        confirm_password.setCustomValidity("You will be redirected shortly");
+        modal.style.display = "block";
+        span.onclick = function() {
+          modal.style.display = "none";
         }
-    }
-}
 
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+
+        }
+        setTimeout(
+          function() {
+            window.location = 'https://appucyriac.github.io/gadget/index.html'
+          }, 2500);
+
+      }
+    }
+  }
+}
