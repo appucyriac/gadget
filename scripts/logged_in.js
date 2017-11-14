c4d19cdd1e6d5459d9274abb05294c890c9b9728
appucyriac <appu@qburst.com>
2017-11-14 11:48:01 +0530

    logout feature

--- scripts/logged_in.js
+++ scripts/logged_in.js
@@ -11,6 +11,10 @@ $(document).ready(function() {
         $(".lin").hide();
         $(".lout").show();
     }
+
+
+
+});
     $("#logout").click(function(event) {
             event.preventDefault();
             alert("You are now logged out");
@@ -18,9 +22,6 @@ $(document).ready(function() {
             $(".lin").show();
             $(".lout").hide();
         });
-
-
-});
 
 
 
