513cda72c515464d855b7f1a89bc72d935e3a514
appucyriac <appu@qburst.com>
2017-11-21 17:04:29 +0530

    content expands on click, like feature comment box

--- scripts/user_functions.js
+++ scripts/user_functions.js
@@ -1,9 +1,12 @@
 var password = document.getElementById("password"),
- confirm_password = document.getElementById("confirm_password"),
- email = document.getElementById("email"),
- modal = document.getElementById('myModal'),
- span = document.getElementsByClassName("close")[0],
- check = /(.+)@(.+){2,}\.(.+){2,}/;
+  confirm_password = document.getElementById("confirm_password"),
+  email = document.getElementById("email"),
+  modal = document.getElementById('myModal'),
+  span = document.getElementsByClassName("close")[0],
+  check = /(.+)@(.+){2,}\.(.+){2,}/,
+  comment =document.getElementsByClassName("commentBox")[0],
+  likeCount =parseInt(localStorage.getItem("like_count"));
+ // likeCount=0;
 
 $(document).ready(function() {
   var usr = localStorage.getItem("signed");
@@ -22,7 +25,8 @@ $(document).ready(function() {
     $(".login-buttons").show();
     $(".logged-buttons").hide();
   });
-      
+  $(".likeClassOne").hide();
+  document.getElementById("counterOne").innerHTML = likeCount;
 });
 
 
@@ -32,12 +36,15 @@ function logIn() {
   if (password.value == "123" && email.value == "appu@qburst.com") {
     successPopup();
     password.setCustomValidity("You will be redirected shortly");
-    if(stay_signed_in.checked){
+    setTimeout(
+      function() {
+        window.location = 'https://appucyriac.github.io/gadget/index.html'
+      }, 2500);
+    if (stay_signed_in.checked) {
       localStorage.setItem("signed", "true");
-    }
-    else{
-          $(".login-buttons").hide();
-          $(".logged-buttons").show();
+    } else {
+      $(".login-buttons").hide();
+      $(".logged-buttons").show();
     }
 
   } else {
@@ -64,6 +71,10 @@ function validatePassword() {
       } else {
         successPopup();
         password.setCustomValidity("You will be redirected shortly");
+        setTimeout(
+          function() {
+            window.location = 'https://appucyriac.github.io/gadget/index.html'
+          }, 2500);
       }
     }
   }
@@ -71,7 +82,7 @@ function validatePassword() {
 
 function successPopup() {
 
-  
+
   modal.style.display = "block";
   span.onclick = function() {
     modal.style.display = "none";
@@ -82,28 +93,67 @@ function successPopup() {
       modal.style.display = "none";
     }
   }
-  setTimeout(
-    function() {
-      window.location = 'https://appucyriac.github.io/gadget/index.html'
-    }, 2500);
+
 }
 
 function loadContent()
+
 {
-   $.getJSON('https://appucyriac.github.io/gadget/json/content.json', function (data) {
-      console.log(data);
-        document.getElementsByClassName("article-title-first")[0].innerHTML=data.title_one;
-        document.getElementsByClassName("first-article")[0].innerHTML=data.article_one;
-        document.getElementsByClassName("article-title-second")[0].innerHTML=data.title_two;
-        document.getElementsByClassName("second-article")[0].innerHTML=data.article_two;
-    });
+  var trimmed_content;
+  $.getJSON('https://appucyriac.github.io/gadget/json/content.json', function(data) {
+    console.log(data);
+    document.getElementsByClassName("article-title-first")[0].innerHTML = data.title_one;
+    trimmed_content = trimContent(data.article_one);
+    document.getElementsByClassName("first-article")[0].innerHTML = trimmed_content;
+    document.getElementsByClassName("article-title-second")[0].innerHTML = data.title_two;
+    trimmed_content = trimContent(data.article_two);
+    document.getElementsByClassName("second-article")[0].innerHTML = trimmed_content;
+  });
 }
 
-function loadAbout()
-{
-     $.getJSON('https://appucyriac.github.io/gadget/json/content.json', function (data) {
-        console.log(data);
-        document.getElementsByClassName("about-content")[0].innerHTML=data.about;
-    });
+function loadAbout() {
+  $.getJSON('https://appucyriac.github.io/gadget/json/content.json', function(data) {
+    console.log(data);
+    document.getElementsByClassName("about-content")[0].innerHTML = data.about;
+  });
 }
 
+function loadFullContent(readMoreId) {
+  $.getJSON('https://appucyriac.github.io/gadget/json/content.json', function(data) {
+    console.log(data);
+    document.getElementsByClassName("article-title-first")[0].innerHTML = data.title_one;
+    if (readMoreId == "read_more_first") {
+      document.getElementsByClassName("first-article")[0].innerHTML = data.article_one;
+      $("#read_more_first").hide();
+      $(".likeClassOne").show();
+    }
+    document.getElementsByClassName("article-title-second")[0].innerHTML = data.title_two;
+    if (readMoreId == "read_more_second") {
+      document.getElementsByClassName("second-article")[0].innerHTML = data.article_two;
+      $("#read_more_second").hide();
+    }
+  });
+}
+
+function trimContent(str) {
+  var sub_str;
+  sub_str = str.substring(0, 250);
+  return sub_str;
+
+}
+
+function likeCounter() {
+  if (localStorage.getItem("signed") == "true") {
+    likeCount += 1;
+    localStorage.setItem("like_count",likeCount);
+    document.getElementById("counterOne").innerHTML = likeCount;
+  } else {
+    successPopup();
+  }
+}
+
+function postComment()
+{
+ localStorage.setItem("comments",comment);
+  document.getElementsByClassName("comments")[0].innerHTML=comment;
+}
